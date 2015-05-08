ccbe0d34f310a2bc9706572f1ada5831b9654373
Martin Maza <Martin@Martins-MacBook-Pro.local>
2015-05-07 17:00:45 -0700

    first commit

---
+++ js/tttApp.js
@@ -0,0 +1,11 @@
+angular.module('tttApp')
+	.controller('Repeater',Repeater);
+
+//object constructor
+function Repeater($scope){
+	$scope.boxes = [1,2,3,4,5,6,7,8,9];
+
+   $scope.addTask = function(){
+      };
+
+}
