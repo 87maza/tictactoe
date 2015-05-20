c3c3c79be4d8459b5ca870bad96dd33c0dc2033d
Martin Maza <Martin@Martins-MacBook-Pro.local>
2015-05-19 18:26:03 -0700

    added comments

--- js/tttApp.js
+++ js/tttApp.js
@@ -5,65 +5,37 @@ angular.module('tttApp')
 Repeater.$inject = ['$firebaseObject', '$scope'];
 
 function Repeater($firebaseObject, $scope){
-// var ref = new Firebase('https://martintttapp.firebaseio.com/gameBoard');
-// $scope.gameBoard = $firebaseObject(ref);
-// // ref.set({
-//   //$scope.gameBoard.boxes = 
-//   "boxes":[{"status": null},{"status": null}, {"status": null},
-//   {"status": null},{"status": null},{"status": null},
-//   {"status": null},{"status": null},{"status": null}];
-//  // $scope.gameBoard.$save();
-// //});
+//Controller as firebaseobject, $scope is used for 3way binding instead of using $save
 
 var ref = new Firebase('https://martintttapp.firebaseio.com');
 $firebaseObject(ref).$bindTo($scope, "game");
+//reference object
+
+var player;
+//player variable used for turnkeeping online click function
+var gameOver = false;
+//gameOver switch for win count and stopping after a winner declared
+$scope.p1Wins = 0;
+$scope.p2Wins = 0;
+//player wins function not ready, need to add another button
 
 
-  var player;
-  
-  var play1 = "X Team";
-  var play2 = "O Team";
-  var playTurn=1;
-  var gameOver = false;
-  $scope.p1Wins = 0;
-  $scope.p2Wins = 0;
-  $scope.symbols = {
-    "1" : "X",
-    "-1": "O"
+ $scope.markSquare = function(square){
+    //markSquare function will mark the proper index with player's value, either 1 or -1
+    if(!$scope.game) {return;}
+   //if not in scope.game get out of function
+    if($scope.game.turn != player){return;}
+     //if statement matches firebase var turn with local var turn to determine whose turn it is online.  it prevents double-play free for all online  
+    if(gameOver){return;}
+    //once winner declared, no one gets to mark on board
+    $scope.game.board[square] = $scope.game.turn;
+    $scope.game.turn *= -1;
+    $scope.getWinner();
+    //turn switch and scan for winner after every turn.
   };
 
-   $scope.markSquare = function(square){
-      
-      if(!$scope.game) {return;}
-     
-      if($scope.game.turn != player){
-       
-        return;
-      }
-      //markSquare function will mark the proper index with player's value, either 1 or -1
-      // var sqVal = $scope.game.board[square];
-      // if(sqVal){return;}
-
-      if(gameOver){return;}
-    
-      // $scope.game.board[square] =  1 : -1;
-
-      // if($scope.game.turn === player){
-      //   console.log(player);
-      //   if (player = 1) {
-      //     $scope.game.board[square] = 1;
-      //   } else if (player = -1) {
-      $scope.game.board[square] = $scope.game.turn;
-        
-      $scope.game.turn *= -1;
-      $scope.getWinner();
-      //} 
-
-      
-    };
-
     $scope.winningCombos = [
-    //winning combos in array of arrays, needs a nested for loop for a call
+    //winning combos in array of arrays, needs a nested for loop isolate winning numbers
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -75,38 +47,37 @@ $firebaseObject(ref).$bindTo($scope, "game");
     ];
 
  $scope.getWinner = function(){
-
-
+    //win logic
     var wc = $scope.winningCombos;
-    console.log("length of winning combos: " + wc.length);
+    //shortened so i dont have to type
     var total = 0;
-
+    //total to gather up winning indices
     for(var i = 0; i < 8; i ++){
         // wc[i] // => [0,1,2]
-        
+        //first for loop used for 8 possible winning combos
       
       for(var j = 0; j < 3; j++){
           // wc[i][j] // => [0,1,2][0] // => 0             
           // wc[i][j] // => [0,1,2][2] // => 2
+          //second for loop used for exact location in winning combos array index
           
-          var windex = wc[i][j]; // (i = 2, j = 2) // index = 7 // $scope.boxes[index]
+          var windex = wc[i][j]; 
+          //winning index numbers from nexted for loops
           
           total += $scope.game.board[windex];
-          // total += $scope.boxes[$scope.winningCombos[i][j]].status
+         // total += $scope.boxes[$scope.winningCombos[i][j]].status
          // console.log($scope.game.board[windex]);
       }
-      if (total === 3){
+      if(total === 3){
+        // winner declared after 3 points, 3 in a row
           $scope.p1Wins += 1;
-          // player 1 wins
-          console.log("player 1 wins");
-          console.log("total wins" + $scope.p1Wins);
+          //win counter incrementer
+          alert("player 1 wins");
           gameOver = true;
       } 
       else if (total === -3){
           $scope.p2Wins += 1;
-          // p2 wins
-          console.log("player 2 wins");
-          console.log("total wins" + $scope.p2Wins);
+          alert("player 2 wins");
           gameOver = true;
       }
       total = 0; //total sets wins to zero, no double counts  
@@ -117,50 +88,39 @@ $firebaseObject(ref).$bindTo($scope, "game");
 
   $scope.clearBoard = function(){
      $scope.game.board = [0,0,0,0,0,0,0,0,0];
-     console.log($scope.game)
       gameOver = false;
       $scope.game.p1Connect = false;
     $scope.game.p2Connect = false;
   };
+  //clears board, sets board to zeroes, starting state/resets player connection
 
   $scope.clearWins = function(){
     $scope.p1Wins = 0;
     $scope.p2Wins = 0;
-    
   };
-  // $scope.winCount = function(){
-  //   if($scope.p1win)
-  // }
+  // clears win board, need to tie this function to a button
   $scope.getSymbol = function(square){
     if(!$scope.game) return;
     var sqVal = $scope.game.board[square];
     if(sqVal){ 
       return (sqVal>0) ? "X" : "O";
+      //ternary statement that states if square value is greater than zero, use x. else use o
     }
   };
+  //allows for function switch -1 and 1's to x and o's
+ 
   $scope.connect = function(){
-  //   $scope.game.p2connect;
-    if ($scope.game.p1Connect === false) {
+    if ($scope.game.p1Connect === false){
       $scope.game.p1Connect = true;     
       player = 1;
-    } else if ($scope.game.p2Connect === false) {
+    } else if ($scope.game.p2Connect === false){
       $scope.game.p2Connect=true;
       player = -1;
     }
-  //     if($scope.game.p2connect === true){
-  //         console.log ("please wait for the next game");
-  //       }else {
-  //         p2connect = true;
-  //         playVal = -1;
-  //       }
-  //  }
-  //  else{
-  //       p1connect = true;
-  //        playVal = 1;
-  //   }
-  // }
+  //connect button that allows for online play assigning priority to first click and using a local variable to assign online player to either x or o permanently.
   }
 }
+//end of controller function
 
 function languages(instruct){
 var e = document.getElementById("instruct");
@@ -175,16 +135,14 @@ var f = document.getElementById("instructeng");
        f.style.display = 'none';
     }
 }
-
+//language switch toggle vanila javascript
 
 /*To do list:
-
-3. CSS / add Icons, Instructions
-4. Add buttons for english instructions
-The object of Tic Tac Toe is to get three in a row. You play on a three by three game board. The first player is known as X and the second is O. Players alternate placing Xs and Os on the game board until either oppent has three in a row or all nine squares are filled.
-5. Add butt
-To ASK:
-2. Player ICON toggle function
-3. When to 
+1. Styling
+2. ADD round counter
+3. Create better Draw Logic
+4. Alert user to announce if its their turn
+5. Timer
+6. Responsive Design
 */
   
